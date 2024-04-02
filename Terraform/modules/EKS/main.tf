ab962bd0c842a3b7099840b0368669511d4c7ed3
Frank Pimienta <frank.pimienta.es@gmail.com>
2024-04-02 19:39:55 +0200

    Update

--- Terraform/modules/EKS/main.tf
+++ Terraform/modules/EKS/main.tf
@@ -4,13 +4,13 @@
 
 # Create log group for EKS cluster
 resource "aws_cloudwatch_log_group" "logs_cluster" {
-  name              = "/aws/eks/${environment_Name}-eks-cluster/cluster"
+  name              = "/aws/eks/${var.environment_Name}-eks-cluster/cluster"
   retention_in_days = 7
 }
 
 # Create IAM role for EKS cluster
 resource "aws_iam_role" "cluster_iam_role" {
-  name = "${environment_Name}-ClusterRole"
+  name = "${var.environment_Name}-ClusterRole"
   assume_role_policy = jsonencode({
     Statement = [{
       Action = "sts:AssumeRole"
@@ -36,7 +36,7 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSVPCResourceController" {
 
 # Create IAM role for EKS pods
 resource "aws_iam_role" "pods_iam_role" {
-  name = "${environment_Name}-PodExecutionRole"
+  name = "${var.environment_Name}-PodExecutionRole"
   assume_role_policy = jsonencode({
     Statement = [{
       Action = "sts:AssumeRole"
@@ -58,8 +58,8 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSFargatePodExecutionRolePolic
 # Create EKS cluster
 resource "aws_eks_cluster" "cluster" {
   depends_on = [aws_cloudwatch_log_group.logs_cluster]
-  name       = "${environment_Name}-eks-cluster"
-  role_arn   = aws_iam_role.cluster.arn
+  name       = "${var.environment_Name}-eks-cluster"
+  role_arn   = aws_iam_role.cluster_iam_role.arn
   vpc_config {
     subnet_ids              = length(var.private_subnet_ids) == 4 ? slice(var.private_subnet_ids, 0, 2) : length(var.private_subnet_ids) == 6 ? slice(var.private_subnet_ids, 0, 2, 4) : var.private_subnet_ids
     security_group_ids      = [var.eks_sg_id]
@@ -68,7 +68,7 @@ resource "aws_eks_cluster" "cluster" {
   }
   enabled_cluster_log_types = ["api", "audit"]
   tags = {
-    "Name" = "${environment_Name}-eks-cluster"
+    "Name" = "${var.environment_Name}-eks-cluster"
   }
 
 }
@@ -77,11 +77,11 @@ resource "aws_eks_cluster" "cluster" {
 # Create Fargate profile
 resource "aws_eks_fargate_profile" "fargate_profile" {
   cluster_name           = aws_eks_cluster.cluster.name
-  fargate_profile_name   = "${environment_Name}-fargate_profile"
+  fargate_profile_name   = "${var.environment_Name}-fargate_profile"
   pod_execution_role_arn = aws_iam_role.pods_iam_role.arn
   subnet_ids             = length(var.private_subnet_ids) == 4 ? slice(var.private_subnet_ids, 0, 2) : length(var.private_subnet_ids) == 6 ? slice(var.private_subnet_ids, 0, 2, 4) : var.private_subnet_ids
 
   selector {
-    namespace = "${environment_Name}-eks"
+    namespace = "${var.environment_Name}-eks"
   }
 }
